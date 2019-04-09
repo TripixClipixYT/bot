f2152e23826092f7bbd56ac2021a9903f36b7eac
TripixClipixYT <49345413+TripixClipixYT@users.noreply.github.com>
2019-04-09 22:09:41 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client(); 
+
+const token = 'NTM1ODU3NTE4MzY3MzQyNTkz.XKUcXw.KUp9crbYBojNcgxua9EegAwI24o';
+const PREFIX = '/';
+
+var version = '1.1.0';
+
+bot.on('ready', () =>{
+      console.log('Tripix Clipix Bot is online!');
+   })
+       
+let statuses =[
+   "Tripix Clipix",
+]
+
+setInterval(function(){
+  let status = statuses[Math.floor(Math.random()* statuses.length)];
+  bot.user.setActivity(status, {type: 'WATCHING'});
+}, 5000)
+
+   bot.on('message', message=>{
+      let args = message.content.substring(PREFIX.length).split(" ");
+      switch(args[0]){
+          case 'ping':
+              message.reply('pong');
+               break;
+                    case 'developer':
+                       message.sendMessage('This is a ChatBot by TripixClipixYT#6035!')
+                             break;
+                                   case 'user':
+                                       const embed = new Discord.RichEmbed()
+                                           .setTitle('User Information')
+                                              .addField('Player Name', message.author.username)
+                                                  .setColor(0xF1C40F)
+                                                    .addField('Current Server', message.guild.name)
+                                                     .setThumbnail(message.author.avatarURL)
+                                                       .setFooter('Made by TripixClipixYT#6035')
+                                                         message.channel.sendEmbed(embed);
+                                                            break;             
+case 'serverinfo':
+    const serverinfo = new Discord.RichEmbed()
+     .setTitle('Server Information')
+        .addField('Server Name', message.guild.name)
+           .setColor(0xF1C40F)
+              .addField('Current Server', message.guild.name)
+                  .setFooter('Made by TripixClipixYT#6035')
+                      message.channel.sendEmbed(embed);
+                           break; 
+      }
+   }
+)         
+bot.login(token);
+ 
