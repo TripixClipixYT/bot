19564c8e3cc19cc2eaecb7e6ce4cfe57bc67a68a
TripixClipixYT <49345413+TripixClipixYT@users.noreply.github.com>
2019-04-25 15:05:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -65,12 +65,12 @@ case 'serverinfo':
                                                    .setThumbnail(bot.user.displayAvatarURL)
                                                    .setTimestamp()
                                                    .setDescription(`These are the avaliable commands at the moment for Tripix Clipix\n The bot prefix is "/"`)
-                                                   .addField(`Commands:\n "developer" "user" "serverinfo" "botinfo" "help"`)
+                                                   .addField(`Commands:\n, "developer" "user" "serverinfo" "botinfo" "help"`)
                                                    .setFooter("Tripix Clipix\n 2k19 Developed!")
                                                    message.channel.send(Sembed);
                                                    break;
                                                    }
-   
+   }
                                              }
 )
 bot.login(token);
