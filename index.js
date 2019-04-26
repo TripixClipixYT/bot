0c01ea4d659244972f4b61281fd8d0e48fa7bcc0
TripixClipixYT <49345413+TripixClipixYT@users.noreply.github.com>
2019-04-26 18:32:06 +0300

    Bot Statuses!
    New Statuses for `MBG` Bot!

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const bot = new Discord.Client();
 
 const token = 'NTM1ODU3NTE4MzY3MzQyNTkz.XMCiQg.pzPztW0RvbvTg1Fd_RSkBLN0d4s';
 const PREFIX = '/';
-
-var version = '1.2.0';
+ 
+var version = '1.1.12';
 
 bot.on('ready', () =>{
       console.log('Tripix Clipix Bot is online!');
    })
 
-   let statuses =["Tripix Clipix and Vanix_'s videos"]
+   let statuses =["Tripix Clipix and Vanix_'s videos"
+       `Over ${bot.guild.size}`,
+       `Over ${bot.user.size}`
+]
 
 setInterval(function(){
   let status = statuses[Math.floor(Math.random()* statuses.length)];
@@ -54,22 +57,51 @@ case 'serverinfo':
                                 .addField('Bot Version', version)
                                    .setColor(0xF1C40F)
                                          .setFooter('Made by TripixClipixYT#6035')
+                                         exports.run = async (client, message, args, tools) => {
+    
+                                            // Variables
+                                            let servers = client.guilds.size; // Server Count
+                                            let users = 0; // Start of user count
+                                            let channels = client.channels.size; // Channel Count
+                                            
+                                            // This goes through every guild to grab an accurate memberCount;
+                                            client.guilds.map(g => users += g.memberCount);
+                                            
+                                            // Form Embed
+                                            const stembed = new Discord.MessageEmbed()
+                                                .setTitle('Community Channels')
+                                                .addField('Servers', servers, true)
+                                                .addField('Users', users, true)
+                                                .addField('Channels', channels, true);
+                                        
+                                            // Send Embed
+                                            
+                                        }
                                              message.channel.sendEmbed(embed4); 
                                                   break;
-                                                    
-    case 'help':
-                                                   
+                                                     
+                                                 case 'help':
+                                                
                                                    const Sembed = new Discord.RichEmbed()
                                                    .setColor(0xF1C40F)
-                                                   .setAuthor(`Tripix Clipix`, message.guild.iconURL)
+                                                   .setAuthor(`MBG`, message.guild.iconURL)
                                                    .setThumbnail(bot.user.displayAvatarURL)
                                                    .setTimestamp()
                                                    .setDescription(`These are the avaliable commands at the moment for Tripix Clipix\n The bot prefix is "/"`)
-                                                   .addField(`Commands:\n, "developer" "user" "serverinfo" "botinfo" "help"`)
+                                                   .addField(`Commands:"developer" "user" "serverinfo" "botinfo"`)
                                                    .setFooter("Tripix Clipix\n 2k19 Developed!")
-                                                   message.channel.send(Sembed);
+                                                   message.channel.send(Sembed)
                                                    break;
-                                                   }
-                                             }
-)
+                                                           
+                                            
+   
+                                         
+                                    
+                                        
+                                     
+                
+      }    
+                               }
+   )
+   
 bot.login(token);
