aa990b60a12abd3e69beff895aafb369e7e24da8
TripixClipixYT <49345413+TripixClipixYT@users.noreply.github.com>
2019-06-05 18:14:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -66,10 +66,47 @@ case 'serverinfo':
                                                    .setThumbnail(bot.user.displayAvatarURL)
                                                    .setTimestamp()
                                                    .setDescription(`These are the avaliable commands at the moment for Tripix Clipix\n The bot prefix is **tr!**`)
-                                                   .addField(`Commands:"developer" "user" "serverinfo" "botinfo" "ping" `)
+                                                   .addField(`Commands:"developer" "user" "serverinfo" "botinfo[Soon...]" "ping[Soon...]" `)
                                                    .setFooter("Tripix Clipix\n 2k19 Developed!")
                                                    message.channel.send(Sembed)
                                                    break;          
+      const { RichEmbed } = require("discord.js")
+const { cyan } = require("../../colours.json");
+const { botconfig } = require ("../../botconfig.json");
+
+var version = "1.0.3[Alpha]";
+var bdeveloper= "LuciferBG#6035";
+
+module.exports = {
+    config: {
+        name: "binfo",
+        description: "Pulls the info of the bot!",
+        usage: "!binfo",
+        category: "miscellaneous",
+        accessableby: "Members",
+        aliases: ["bi", "botdesc"]
+    },
+    run: async (bot, message, args) => {
+    let biEmbed = new RichEmbed()
+        .setColor(cyan)
+        .setTimestamp()
+        .setAuthor(bot.user.username, bot.user.displayAvatarURL)
+        .setThumbnail(bot.user.displayAvatarURL)
+        .setTitle("**Bot Info**")
+        .addField("**Bot Name**", bot.user.username)
+        .addField("**Bot's Website**", "https://lucifers-coding.site123.me")
+        .addField("**Bot Owner:**", bdeveloper)
+        .addField("**Bot Version:**", version)
+        .setDescription("Tripix's Bot Information")
+        .addField("**The bot was created on:**", bot.user.createdAt)
+        .addField("**Bot Type:**", "_Chat Bot_")
+        .addField("**Total Servers:**", bot.guilds.size)
+        .addField("**More Info:**", "When the bot is in **Beta** mode **admins** and **mods** can use the bot code and help for finishig him!")
+        .setFooter(`Gamerz`, bot.user.displayAvatarURL);
+    message.channel.send(biEmbed);
+    }
+}
+      
       } 
         if (message.content === prefix + "ping"){
     message.reply("Pong! (hold on, processing latency...)").then(m => m.edit(`${message.author}:ping_pong: Pong!  (Current latency is ${m.createdTimestamp - message.createdTimestamp}ms, while the API Latency is ${Math.round(bot.ping)}ms)`) );
